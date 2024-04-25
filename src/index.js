e46d9209ff8acc47579f96df8c9a8ee0b4d07662
Shaamil Karim <40500380+shaamil101@users.noreply.github.com>
2024-04-24 20:40:51 -0400

    part 1

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import $ from 'jquery';
-import './style.scss';
-let seconds = 0;
-        
-setInterval(() => {
-  seconds++;
-  $('#main').html(`You've been on this page for ${seconds} seconds.`);
-}, 1000);
--- src/index.jsx
+++ src/index.jsx
@@ -1,51 +1,54 @@
 /* eslint-disable react/function-component-definition */
-/* eslint-disable react/react-in-jsx-scope */
 import React from 'react';
+import './style.scss'; 
 import { createRoot } from 'react-dom/client';
 import {
   BrowserRouter, Routes, Route, NavLink, useParams,
- } from 'react-router-dom';
- const About = (props) => {
-   return <div> All there is to know about me </div>;
- };
- const Welcome = (props) => {
-   return <div>Welcome</div>;
- };
- const Test = (props) => {
+} from 'react-router-dom';
+import Counter from './components/counter';
+import Controls from './components/controls';
+
+const About = (props) => {
+  return <div> All there is to know about me </div>;
+};
+const Welcome = (props) => {
+  return <div>Welcome <Counter /> <Controls /> </div>;
+};
+const Test = (props) => {
   const { id } = useParams();
   return <div> ID: {id} </div>;
 };
 const FallBack = (props) => {
   return <div>URL Not Found</div>;
 };
- const App = (props) => {
-   return (
-     <BrowserRouter>
-       <div>
-         <Nav />
-         <Routes>
-           <Route path="/" element={<Welcome />} />
-           <Route path="/about" element={<About />} />
-           <Route path="/test/:id" element={<Test />} />
-           <Route path="*" element={<FallBack />} />
-         </Routes>
-       </div>
-     </BrowserRouter>
-   );
- };
+const App = (props) => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/test/:id" element={<Test />} />
+          <Route path="*" element={<FallBack />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+};
  
- const Nav = (props) => {
-   return (
-     <nav>
-       <ul>
-         <li><NavLink to="/">Home</NavLink></li>
-         <li><NavLink to="/about">About</NavLink></li>
-         <li><NavLink to="/test/id1">test id1</NavLink></li>
-         <li><NavLink to="/test/id2">test id2</NavLink></li>
-       </ul>
-     </nav>
-   );
- }; 
+const Nav = (props) => {
+  return (
+    <nav>
+      <ul>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/about">About</NavLink></li>
+        <li><NavLink to="/test/id1">test id1</NavLink></li>
+        <li><NavLink to="/test/id2">test id2</NavLink></li>
+      </ul>
+    </nav>
+  );
+}; 
 
 const root = createRoot(document.getElementById('main'));
 root.render(<App />);
